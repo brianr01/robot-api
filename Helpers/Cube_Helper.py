c22152097bd92558d7eafe20f15c99faf5d4a94a
brianr01 <brianr.email1@gmail.com>
2021-01-21 20:52:57 -0500

    updated cube helper

--- Helpers/Cube_Helper.py
+++ Helpers/Cube_Helper.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import request
-import sys
+import sys, random, time
 
 sys.path.append(sys.path[0] + '/Helpers')
 from Motors_Helper import Motors_Helper
@@ -20,6 +20,49 @@ class Cube_Helper:
     def Power(self, state):
         self.motors.Append_Power_To_Action_String(state) 
 
-
     def Execute(self):
         self.motors.Send_Action_String()
+
+    def Do_Random_Moves_While_Tracking_Cube(self, move_count):
+        for i in range(0, move_count):
+            self.Append_Random_Move()
+        self.Execute()
+        time.sleep(move_count * .15)
+
+    def Append_Random_Move(self):
+        self.Turn(
+            self.Get_Random_Side(),
+            self.Get_Random_Direction()
+        )
+
+    def Get_Random_Side(self):
+        return random.choice('urfdlb')
+
+    def Get_Random_Direction(self):
+        return random.choice(['c', 'ccw'])
+
+    def Solve_Cube_With_Tracked_Position(self):
+        solution = self.virtual_cube.Get_Solution()
+        
+        self.Execute_Moves_With_Normal_Notation(solution)
+
+    def Execute_Moves_With_Normal_Notation(self, moves):
+        moves = moves.split(" ")
+
+        self.Power('on')
+
+        for move in moves:
+            self.Execute_Single_Move_With_Normal_Notation(move)
+        
+        self.Power('off')
+        self.Execute()
+
+    def Execute_Single_Move_With_Normal_Notation(self, move):
+        direction = 'c'
+        if "'" in move:
+            direction = 'ccw'
+
+        if "2" in move:
+            self.Turn(move[0].lower(), direction)
+
+        self.Turn(move[0].lower(), direction)
