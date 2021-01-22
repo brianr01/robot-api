296f67dfe906e5dbe897083ca32e26e305aa0fa2
brianr01 <brianr.email1@gmail.com>
2021-01-21 20:52:31 -0500

    updated cube controller

--- Controllers/Cube_Controller.py
+++ Controllers/Cube_Controller.py
@@ -1,13 +1,21 @@
 from flask import Blueprint
 from flask import request
-import serial, sys, random
-sys.path.append(sys.path[0] + '/Helpers')
-from Turn_Controller import cube
+import serial, random
 
+import sys
+sys.path.append(sys.path[0] + '/Helpers')
+from Robot_Helper import robot
 
 cube_controller = Blueprint('cube_controller', __name__)
 
 @cube_controller.route("/get_cube_position")
 def Get_Cube_Position():
-    global cube
-    return cube.virtual_cube.Get_Cube_State()
+    global robot
+    return robot.cube.virtual_cube.Get_Cube_State()
+
+@cube_controller.route("/generate_training_images")
+def Generate_Cube_Training_Images():
+    global robot
+
+    robot.Generate_Cube_Training_Images()
+    return 'Done'
